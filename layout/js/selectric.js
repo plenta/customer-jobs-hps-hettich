e8f61f6b8fb21f21b4e24abd217f8a3bbae06b4e
Christoph Werner <christopwerner@gmx.at>
2022-09-15 15:55:56 +0200

    *

--- layout/js/selectric.js
+++ layout/js/selectric.js
@@ -1,12 +1,13 @@
 import $ from 'jquery';
 
 document.addEventListener('DOMContentLoaded', function () {
-    const $select = $('select');
+    const $select = $('#container select');
     if ($select.length) {
         import('selectric/src/jquery.selectric').then((selectric) => {
             $select.selectric({
                 disableOnMobile: false,
             });
+
             $('.selectric-wrapper').parentsUntil('.inside').each(function () {
                 if ($(this).hasClass('block')) {
                     $(this).addClass('force-overflow-visible');
